2f4e48c0dddd1b5045327e4ae158d82e2c0448de
Baochuan Lu <lubaochuan@gmail.com>
2019-11-15 13:51:29 -0600

    use visibility filter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import List from "./List";
 import Input from "./Input";
 import Title from "./Title";
+import { VisibilityFilters } from "./constants";
+import Footer from "./Footer";
 
 export default class App extends Component {
   key = 0;
@@ -11,13 +13,15 @@ export default class App extends Component {
       { text: "eat", id: this.key++, completed: false },
       { text: "drink", id: this.key++, completed: false },
       { text: "be merry", id: this.key++, completed: false }
-    ]
+    ],
+    visibilityFilter: VisibilityFilters.SHOW_ALL
   };
 
   onAddTodo = text => {
     const { todos } = this.state;
 
     this.setState({
+      ...this.state,
       todos: [{ text, id: this.key++, completed: false }, ...todos]
     });
   };
@@ -38,6 +42,14 @@ export default class App extends Component {
     });
   };
 
+  onUpdateVisibilityFilter = visibility => {
+    console.log("new visibility: " + visibility);
+    this.setState({
+      ...this.state,
+      visibilityFilter: visibility
+    });
+  };
+
   onRemoveTodo = index => {
     const { todos } = this.state;
     /*
@@ -48,10 +60,14 @@ export default class App extends Component {
   };
 
   render() {
-    const { todos } = this.state;
+    const { todos, visibilityFilter } = this.state;
 
-    const activeTodos = todos.filter(todo => !todo.completed);
-    const completedTodos = todos.filter(todo => todo.completed);
+    let visibleTodos = todos;
+    if (visibilityFilter === VisibilityFilters.SHOW_ACTIVE) {
+      visibleTodos = todos.filter(todo => !todo.completed);
+    } else if (visibilityFilter === VisibilityFilters.SHOW_COMPLETED) {
+      visibleTodos = todos.filter(todo => todo.completed);
+    }
 
     return (
       <div style={styles.container}>
@@ -60,9 +76,11 @@ export default class App extends Component {
           placeholder={"Type a todo, then hit enter!"}
           onSubmitEditing={this.onAddTodo}
         />
-        <List list={activeTodos} onToggleTodo={this.onToggleTodo} />
-        <Title>Completed List</Title>
-        <List list={completedTodos} onToggleTodo={this.onToggleTodo} />
+        <List list={visibleTodos} onToggleTodo={this.onToggleTodo} />
+        <Footer
+          currentFilter={this.state.visibilityFilter}
+          onUpdateVisibilityFilter={this.onUpdateVisibilityFilter}
+        />
       </div>
     );
   }
