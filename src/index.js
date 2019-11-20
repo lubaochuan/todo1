2922a02c568f03930f91866c4733c57a4c5a9702
Baochuan Lu <lubaochuan@gmail.com>
2019-11-20 11:40:52 -0600

    add logger as redux middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
 
 // Import the reducer and create a store
 import { reducer } from "./TodoListRedux";
-const store = createStore(reducer);
+
+const logger = store => next => action => {
+  console.log("dispatching", action);
+  let result = next(action);
+  console.log("next state", store.getState());
+  return result;
+};
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 const AppWithStore = (
   <Provider store={store}>
